4064fddf01d25978bbae82db8538bbde9768ff25
danielmrz7 <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>
2026-04-21 10:02:10 -0300

    Feat: Add carregamento de dados para a plataforma de controle de versões

--- pre-processing/main.py
+++ pre-processing/main.py
@@ -26,5 +26,37 @@ low_memory = False
 print(f"Shape:{df_raw.shape}")
 print(f"Columns:{list(df_raw.columns[:10])}")
 
+#inicializa o carregamento dos dados
+#job_type é a tarefa q vai ser feita, ou seja, vai carregar todos os dados
+#nomeia o projeto
+wandb.init(
+    project="weather-classification",
+    job_type="load_raw",
+    name="load_weather_data"
+)
+#vai criar um artefato para versionar e para armazenar os dados
+#cria a categoria
+#e adiciona a descrição
+artifact = wandb.Artifact (
+    name = "raw_data", 
+    type = "dataset",
+    description = "Wheater Type raw dataset from Kaggle"
+)
+#nome do arquivo temporário
+#salva o dataframe
+#manda o arquivo csv pro artefato
+temp_path = "temp_raw.csv"
+df_raw.to_csv(temp_path, index=False )
+artifact.add_file(temp_path)
+
+#envia o artefato para o wandb
+wandb.log_artifact(artifact)
+
+
+wandb.summary["rows"]= len(df_raw) #lista as linhas   
+wandb.summary["columns"]= list(df_raw.columns) #lista os nomes das colunas
+
+#finaliza o carregamento
+wandb.finish()
 
 
